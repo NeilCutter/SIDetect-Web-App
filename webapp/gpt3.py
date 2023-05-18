8d5b361fd8451d310027edf19760f1379fd33f59
NeilCutter <neilcanas08@gmail.com>
2023-05-18 10:15:42 +0800

    new changes

--- webapp/gpt3.py
+++ webapp/gpt3.py
@@ -5,7 +5,7 @@ import os
 # openai.api_key = os.environ.get('KEY')
 
 #For localhost
-openai.api_key = "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"
+openai.api_key = "add api key here"
 
 def text_process(prompt, engine="text-davinci-003", temperature=0.6, max_tokens=2000, top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0):
     response = openai.Completion.create(
